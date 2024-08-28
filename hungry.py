14b698667b672a70c9a6504d9c7f6f4a536f4aa4
hasanpy-123 <rana.hasan.adan@gmail.com>
2024-08-28 11:49:44 -0700

    burger

--- hungry.py
+++ hungry.py
@@ -1,6 +1,7 @@
 hungry = input("are you hungry?")
-if hungry == "yes":
+if hungry == "yes" and hungry == "yo":
     print("eat samosa")
     print('eat pizza')
+    print('eat burger')
 else:
     print("do homework")
